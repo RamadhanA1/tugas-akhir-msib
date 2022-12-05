b1c3f1a4750787512674c2f46a74b7ff9e69e530
RamadhanA1 <ramadhanamannu103@gmail.com>
2022-12-05 18:49:50 +0700

    bismillah permulaan

---
+++ index.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <title>CompStore - Belanja Disini Aja!</title>
+</head>
+
+<body>
+
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container">
+      <a href="#" class="navbar-brand">CompStore</a>
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link active">Product</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link active">FAQ</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link active">Contact Us</a>
+          </li>
+        </ul>
+        <div class="d-flex">
+          <button type="submit" class="btn btn-success me-2">Login</button>
+          <button type="submit" class="btn btn-outline-success">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Hero -->
+  <div class="p-5 text-center bg-image rounded-3" style="
+        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('img/hero-2.jpg');
+        height: 70vh;
+      ">
+    <div class="mask container">
+      <div class="d-flex justify-content-left align-items-center h-100 text-start">
+        <div class="text-white">
+          <h1 class="mb-3">Sale Of The Day!</h1>
+          <h4 class="mb-3">Pesan sekarang dan dapatkan potongan hingga 30%</h4>
+          <!-- <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a> -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Categories -->
+  <section class="container mt-5 text-center align-items-center d-flex">
+      <div class=" ">
+        <h1 class="fs-2 fw-bold" sty>Layanan Kami</h1>
+        <br />
+        <div class="row">
+          <div class="col-3 ms-auto" style="width: 16rem">
+            <div
+              class="rounded-5 card p-3 shadow"
+              style="width: 16rem; min-height: 22rem"
+            >
+              <img
+                style="width: 8rem"
+                src=""
+                class="card-img-top p-3"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Servis Komputer</h5>
+                <p class="card-text">
+                  Tambah RAM, Ganti Harddisk, Servis Motherboard, Cleaning CPU,
+                  Bluescreen, Install Aplikasi dan Windows
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 ms-auto" style="width: 16rem">
+            <div
+              class="rounded-5 card p-3 shadow"
+              style="width: 16rem; min-height: 22rem"
+            >
+              <img
+                style="width: 8rem"
+                src=""
+                class="card-img-top p-3"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Servis Komputer</h5>
+                <p class="card-text">
+                  Tambah RAM, Ganti Harddisk, Servis Motherboard, Cleaning CPU,
+                  Bluescreen, Install Aplikasi dan Windows
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 ms-auto" style="width: 16rem">
+            <div
+              class="rounded-5 card p-3 shadow"
+              style="width: 16rem; min-height: 22rem"
+            >
+              <img
+                style="width: 8rem"
+                src=""
+                class="card-img-top p-3"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Servis Komputer</h5>
+                <p class="card-text">
+                  Tambah RAM, Ganti Harddisk, Servis Motherboard, Cleaning CPU,
+                  Bluescreen, Install Aplikasi dan Windows
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 ms-auto" style="width: 16rem">
+            <div
+              class="rounded-5 card p-3 shadow"
+              style="width: 16rem; min-height: 22rem"
+            >
+              <img
+                style="width: 8rem"
+                src=""
+                class="card-img-top p-3"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Servis Komputer</h5>
+                <p class="card-text">
+                  Tambah RAM, Ganti Harddisk, Servis Motherboard, Cleaning CPU,
+                  Bluescreen, Install Aplikasi dan Windows
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <!-- <h1>Hello, world!</h1> -->
+
+
+
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+  <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
